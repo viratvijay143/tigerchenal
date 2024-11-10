892a6da9d0b2855b36f56b23d140dbd0c45ce59c
SAHU <161798305+viratvijay143@users.noreply.github.com>
2024-11-10 06:17:18 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
 api_id = 20047839
 api_hash = "e635f85a4dae812a26c450c0d41276b0"
-bot_token = "7261086621:AAFTjL9QjILoXUxHvhyrIeHJmdDUPP1boqw"
+bot_token = "7261086621:AAGXBRshSj2opnkTQczP_lyKqPK-LouCsOc"
 
